18a24cd791f026430da6a04a975c422ad94447ae
Star <125512934+StaryLOL@users.noreply.github.com>
2024-04-24 16:28:36 -0700

    Update SaveManager.lua

--- bin/SaveManager.lua
+++ bin/SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService("HttpService")
 
 local SaveManager = {} do
-	SaveManager.Folder = "Levitate."
+	SaveManager.Folder = "LevitateSettingTest"
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
