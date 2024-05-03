2de7b8eaa0feeae0fb55521de21f33078f3bda30
Star <125512934+StaryLOL@users.noreply.github.com>
2024-05-02 19:56:09 -0700

    Create LevitateLoader.lua

---
+++ LevitateLoader.lua
@@ -0,0 +1,14 @@
+if game.PlaceId == 8542259458 or game.PlaceId == 8542275097 or game.PlaceId == 8951451142 then -- skywars
+
+   loadstring(game:HttpGet("a", true))()
+
+
+elseif game.PlaceId == 11630038968 or game.PlaceId == 12011959048 then -- bridgeduels
+
+   loadstring(game:HttpGet("a", true))()
+
+else 
+ 
+    print("womp womp")
+  
+end
