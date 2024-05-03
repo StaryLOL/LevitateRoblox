f47af8401ed3c18b3baf2f610cdc2cef9203934a
Star <125512934+StaryLOL@users.noreply.github.com>
2024-05-02 20:02:41 -0700

    Update LevitateLoader.lua

--- LevitateLoader.lua
+++ LevitateLoader.lua
@@ -1,11 +1,11 @@
 if game.PlaceId == 8542259458 or game.PlaceId == 8542275097 or game.PlaceId == 8951451142 then -- skywars
 
-   loadstring(game:HttpGet("a", true))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/StaryLOL/LevitateRoblox/main/games/skywars.lua", true))()
 
 
 elseif game.PlaceId == 11630038968 or game.PlaceId == 12011959048 then -- bridgeduels
 
-   loadstring(game:HttpGet("a", true))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/StaryLOL/LevitateRoblox/main/games/bridgeduels.lua", true))()
 
 else 
  
